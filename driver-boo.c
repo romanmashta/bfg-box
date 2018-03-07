9642825e3c869e540337b6b623a8eda1263c3ef7
Roman Mashta <roman.mashta@gmail.com>
2018-03-07 03:36:49 +0200

    FPGA bridge added.

--- driver-boo.c
+++ driver-boo.c
@@ -39,6 +39,7 @@
 #if defined(unix)
 	#include <errno.h>
 	#include <fcntl.h>
+	#include <sys/mman.h>
 #endif
 
 BFG_REGISTER_DRIVER(boo_drv)
@@ -58,7 +59,7 @@ static int boo_autodetect()
 		cgpu->drv = &boo_drv;
 		cgpu->deven = DEV_ENABLED;
 		cgpu->threads = 1;
-		// cgpu->kname = "BOO";
+
 		add_cgpu(cgpu);
 	}
 	return opt_n_threads;
@@ -66,51 +67,102 @@ static int boo_autodetect()
 
 static void boo_detect()
 {
-	printf("boo_detect\n");
 	noserial_detect_manual(&boo_drv, boo_autodetect);
 }
 
 static
 float boo_min_nonce_diff(struct cgpu_info * const proc, const struct mining_algorithm * const malgo)
 {
-	printf("boo_min_nonce_diff\n");
 	return minimum_pdiff;
 }
+
+#define HPS_TO_FPGA_LW_BASE 0xFF200000
+#define HPS_TO_FPGA_LW_SPAN 0x0100000
 
 static
 bool scanhash_generic(struct thr_info * const thr, struct work * const work, const uint32_t max_nonce, uint32_t * const last_nonce, uint32_t n)
 {
-	struct mining_algorithm * const malgo = work_mining_algorithm(work);
-	void (* const hash_data_f)(void *, const void *) = malgo->hash_data_f;
-	uint8_t * const hash = work->hash;
+    void * lw_bridge_map = 0;
+    int devmem_fd = 0;    
+    devmem_fd = open("/dev/mem", O_RDWR | O_SYNC);
+    lw_bridge_map = (uint32_t*)mmap(NULL, HPS_TO_FPGA_LW_SPAN, PROT_READ|PROT_WRITE, MAP_SHARED, devmem_fd, HPS_TO_FPGA_LW_BASE);
+
+    uint32_t *base = (uint32_t*)(lw_bridge_map + 0x50000);
+    uint32_t *id = (base + 0x42);
+    uint32_t *start = (base + 0x20);
+    uint32_t *busy = (base + 0x20);
+    uint32_t *got_ticket = (base + 0x21);
+
+    uint32_t *g_nonce = (base + 0x22);
+    uint32_t *nonce = (base + 0x23);
+    uint32_t *nonce_start = (base + 0x25);    
+
+	uint8_t * const midstate = work->midstate;
 	uint8_t *data = work->data;
 	const uint8_t * const target = work->target;
 	uint32_t * const out_nonce = (uint32_t *)&data[0x4c];
 	bool ret = false;
 	
-	const uint32_t hash7_targ = le32toh(((const uint32_t *)target)[7]);
-	uint32_t * const hash7_tmp = &((uint32_t *)hash)[7];
-	
-	while (true)
+	uint8_t ob_bin[64];
+	uint32_t *udata = (uint32_t *)ob_bin;
+	memset(ob_bin, 0, sizeof(ob_bin));
+
+	/*
+	char *g=
+			"4679ba4ec99876bf4bfe086082b40025"
+			"4df6c356451471139a3afa71e48f544a"
+			"00000000000000000000000000000000"
+			"0000000087320b1a1426674f2fa722ce";
+	hex2bin(ob_bin, g, sizeof(ob_bin));
+	*/
+
+	swab256(ob_bin, work->midstate);
+	bswap_96p(&ob_bin[0x34], &work->data[0x40]);
+
+	*(base + 0x00) = swab32(udata[7] );
+	*(base + 0x01) = swab32(udata[6] );
+	*(base + 0x02) = swab32(udata[5] );
+	*(base + 0x03) = swab32(udata[4] );
+	*(base + 0x04) = swab32(udata[3] );
+	*(base + 0x05) = swab32(udata[2] );
+	*(base + 0x06) = swab32(udata[1] );
+	*(base + 0x07) = swab32(udata[0] );
+
+	*(base + 0x10) = swab32(udata[15] );
+	*(base + 0x11) = swab32(udata[14] );
+	*(base + 0x12) = swab32(udata[13] );
+	*(base + 0x13) = swab32(udata[12] );
+	*(base + 0x14) = swab32(udata[11] );
+	*(base + 0x15) = swab32(udata[10] );
+	*(base + 0x16) = swab32(udata[9] );
+	*(base + 0x17) = swab32(udata[8] );
+
+	*nonce_start = n;
+
+	*start = 1;
+	while (*busy==1)
 	{
-		*out_nonce = n;
+		*out_nonce = *nonce;
 		
-		/*
-		hash_data_f(hash, data);
-		
-		if (unlikely(le32toh(*hash7_tmp) <= hash7_targ))
-		{
-			ret = true;
+		usleep(100);
+		if (thr->work_restart){
 			break;
-		}*/
-
-		if ((n >= max_nonce) || thr->work_restart)
-			break;
-
-		n++;
+		}
 	}
+
+	if(*got_ticket == 1 && !thr->work_restart){
+		ret = true;
+		*last_nonce = *g_nonce;
+		*out_nonce = *g_nonce;
+	}
+	else{
+		*last_nonce = max_nonce;
+		*out_nonce = max_nonce;
+	}
+
+	munmap(lw_bridge_map, HPS_TO_FPGA_LW_SPAN);
+	close(devmem_fd);
 	
-	*last_nonce = n;
 	return ret;
 }
 
@@ -152,7 +204,6 @@ BOOSearch:
 
 static bool boo_thread_prepare(struct thr_info *thr)
 {
-	printf("boo_thread_prepare\n");
 	struct cgpu_info *cgpu = thr->cgpu;
 	
 	thread_reportin(thr);
@@ -160,16 +211,8 @@ static bool boo_thread_prepare(struct thr_info *thr)
 	return true;
 }
 
-/*
-static uint64_t boo_can_limit_work(struct thr_info __maybe_unused *thr)
-{
-	printf("boo_can_limit_work\n");
-	return 0xffff;
-}*/
-
 static bool boo_thread_init(struct thr_info *thr)
 {
-	printf("boo_thread_init\n");
 	const int thr_id = thr->id;
 	struct cgpu_info *cgpu = thr->cgpu;
 
@@ -183,7 +226,6 @@ struct device_drv boo_drv = {
 	.drv_min_nonce_diff = boo_min_nonce_diff,
 	.drv_detect = boo_detect,
 	.thread_prepare = boo_thread_prepare,
-	// .can_limit_work = boo_can_limit_work,
 	.thread_init = boo_thread_init,
 	.scanhash = boo_scanhash,
 };
